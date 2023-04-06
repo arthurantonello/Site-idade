beb0da019959c17d95366792caf9fc030176e711
Arthur Antonello <94024050+arthurantonello@users.noreply.github.com>
2023-04-06 19:54:07 -0300

    Add files via upload
    Aparição das imagens resolvida

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+let data = new Date()
+let ano = data.getFullYear()
+let formAno = document.querySelector('input#txtano')
+let img = document.createElement('img')
+let res = document.querySelector('div.res')
 function verificar(){
-    let data = new Date()
-    let ano = data.getFullYear()
-    let formAno = document.querySelector('input#txtano')
-    let res = document.querySelector('div.res')
-    res.innerHTML = `Detectamos: ${sexo()} de ${idade(ano, formAno)} anos.`
+    res.innerHTML = `Detectamos: ${sexo()} de ${idade()} anos.`
     imagem()
 }
 
-function idade(ano, formAno){
+function idade(){
     let res = document.querySelector('div.res')
     if (formAno.value.length == 0 || formAno.value.length < 4|| Number(formAno.value) > ano){
         alert('[ERRO]Verifique os dados e tente novamente')
@@ -21,7 +22,6 @@ function sexo(){
     let res = document.querySelector('div.res')
     let fsex = document.getElementsByName('radsex')
     let genero = ''
-    let img = document.createElement('img')
     img.setAttribute('id', 'foto')
 
     if (fsex[0].checked){
@@ -35,29 +35,29 @@ function sexo(){
 
 //ideia (precisa resolver a questao da idade)
 function imagem(){
-    let idadepessoa = idade(this.ano, this.formAno)
+    let idadepessoa = idade()
     if (sexo() == 'Homem'){
         if(idadepessoa >= 0 && idadepessoa < 12){
-            img.setAttribute('src', 'assets/img/homem-bebe.png')
+            fotografia.setAttribute('src', 'assets/img/homem-bebe.png')
         }
         else if(idadepessoa < 21) {
-            img.setAttribute('src', 'assets/img/homem-jovem.png')
+            fotografia.setAttribute('src', 'assets/img/homem-jovem.png')
         }else if(idadepessoa >= 21 && idadepessoa < 60){
-            img.setAttribute('src', 'assets/img/homem-adulto.png')
+            fotografia.setAttribute('src', 'assets/img/homem-adulto.png')
         }else{
-            img.setAttribute('src', 'assets/img/homem-idoso.png')
+            fotografia.setAttribute('src', 'assets/img/homem-idoso.png')
         }
     }else{
         if(idadepessoa >= 0 && idadepessoa < 12){
-            img.setAttribute('src', 'assets/img/mulher-bebe.png')
+            fotografia.setAttribute('src', 'assets/img/mulher-bebe.png')
         }
         else if(idadepessoa < 21) {
-            img.setAttribute('src', 'assets/img/mulher-jovem.png')
+            fotografia.setAttribute('src', 'assets/img/mulher-jovem.png')
         }else if(idadepessoa >= 21 && idadepessoa < 60){
-            img.setAttribute('src', 'assets/img/mulher-jovem.png')
+            fotografia.setAttribute('src', 'assets/img/mulher-jovem.png')
         }else{
-            img.setAttribute('src', 'assets/img/mulher-idosa.png')
+            fotografia.setAttribute('src', 'assets/img/mulher-idosa.png')
         }
     }
-    res.appendChild(img)
 }
+
